abd0d404556a455a0812f82a006ea020f5a1b286
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-04-23 01:10:43 -0400

    added base of the node code

---
+++ src/component/navbar/navbar.js
@@ -0,0 +1,30 @@
+import React from "./node_modules/react";
+
+function NavBar() {
+  return (
+    <nav class="navbar navbar-light navbar-expand-lg">
+
+        <a class="navbar-brand text-white" href="index.html">JacquelineRivera</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar1">
+            
+            <ul class="navbar-nav ml-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="assets/contact.html">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="assets/portfolio.html">Portfolio</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">About</a>
+            </li>
+
+            </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default NavBar;
