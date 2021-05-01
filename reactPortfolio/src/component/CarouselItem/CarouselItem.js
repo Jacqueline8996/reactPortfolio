619f032d8cd6a9de4221ade96796bb2960a8ff69
Jacqueline Rivera <Jacqueline8996@gmail.com>
2021-04-30 20:53:16 -0400

    added in new Image

--- reactPortfolio/src/component/CarouselItem/CarouselItem.js
+++ reactPortfolio/src/component/CarouselItem/CarouselItem.js
@@ -7,6 +7,8 @@ import ThirdImage from "../../assets/Images/RetroBurger.png";
 import FourImage from "../../assets/Images/WeatherDash.png";
 import FithImage from "../../assets/Images/QuizStart.png";
 import SixImage from"../../assets/Images/noteTaker.png";
+import SevenImage from"../../assets/Images/DemoTeam.png";
+
 const SLIDE = [
     { alt:"First slide",title:'Lets Plan A Day',href:"https://github.com/nvandenberge/LetsPlanADay",src:`${FirstImage}`,Description:"Enter a locartion to get a random location and food", state:" carousel-item active"},
     { alt:"Second slide",title:'Pokemon Card game',href:"https://vast-forest-11479.herokuapp.com/",src:`${SecondImage}`,Description:"Demonstrated the base information of CRUD by utilizing a popular card-game in order to create personalized decks for each individual user that can be updated, modified and deleted.",state:"carousel-item"},
@@ -14,7 +16,7 @@ const SLIDE = [
     { alt:"Fourth slide",title:'Weather Dashboard',href:"https://github.com/nvandenberge/LetsPlanADay",src:`${FourImage}`,Description:"A Weather dashboard where you can place your zip code and get a daily and a 5day forcast ",state:"carousel-item"},
     { alt:"Fith slide",title:'Quiz game',href:"https://jacqueline8996.github.io/Homework4/",src:`${FithImage}`,Description:"A Timead Quiz used to test knowledge of coding and will be keeping the score of the user.",state:"carousel-item"},
     { alt:"Six slide",title:'Note Taker',href:"https://frozen-journey-71877.herokuapp.com/",src:`${SixImage}`,Description:"A responsive Note taking application",state:"carousel-item"},
-    // { alt:"First slide",title:'Lets Plan A Day',href:"https://github.com/nvandenberge/LetsPlanADay",src:`{FirstImage}`,Description:"Enter a locartion to get a random location and food"},
+    { alt:"Seven slide",title:'Team Profile',href:"https://github.com/Jacqueline8996/TeamProfile",src:`${SevenImage}`,Description:"Using node moduels you are able to create a team profile"},
 ]
 
 class CarouselItem extends React.Component {
